94b42d17d472403ae76b7efe88c78218bc6f0fb2
Samuel Santos <82621923+samur2d2@users.noreply.github.com>
2021-06-20 04:35:03 +0000

    adicionei a funcao pra receber o valor do investimento. falhei 2 vezes tentando fazer uma outra coisa.

--- investimento.h
+++ investimento.h
@@ -1,8 +1,23 @@
 #ifndef tInvestimento_h
 #define tInvestimento_h
 
+using namespace std;
+
 typedef struct{
 
+    float ChoosingAction(){
+        float startingCapital=0;
+        system("cls");
+        for (int i=0; i<3; i++){cout << endl;}
+        cout << "\t +------------------------------------------------+" << endl;
+        cout << "\t |                                                |" << endl;
+        cout << "\t |         PLEASE, TELL ME HOW MUCH YOU           |" << endl;
+        cout << "\t |           HAVE TO INVEST HIGH NOW              |" << endl;
+        cout << "\t |                                                |" << endl;
+        cout << "\t +------------------------------------------------+" << endl;
+        cout << "\t   I have: R$ "; cin >> startingCapital;
+        return (startingCapital);
+    }
 }tInvestimento;
 
 #endif
